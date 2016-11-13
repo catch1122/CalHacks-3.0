107648d34a36af40e948af30a601a11199dec04c
Joanne Kwon <dayoungk@usc.edu>
2016-11-12 21:45:58 -0800

    Amadeus Scripts complete

--- PeriTrip/Assets/amadeus.cs
+++ PeriTrip/Assets/amadeus.cs
@@ -1,15 +1,109 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
 
-public class amadeus : MonoBehaviour {
+public class Amadeus : MonoBehaviour {
+
+	public string url;
+
+	public string apikey = "5ydifrkNfmgRtCgp7lGDA4GYuOtsUlHG";
+	public string origin;
+	public string one_way = "false";
+	public string duration;
+	public string direct = "true";
+	public string max_price;
+
+//	public Query q;	// CHANGE LIKE IT SHOULD BE SET UP HERE
+	private Query q;
+
+	public List<string> destinations;
 
 	// Use this for initialization
-	void Start () {
+	IEnumerator Start () {
 		GetComponent<Renderer> ().material.color = new Color(0.5f, 1, 1, 0.1f); //C#
+
+		destinations = new List<string> ();
+
+		q = GameObject.Find("Sphere").GetComponent<Query>();
+
+//		Debug.Log ("Hello");
+
+		// will be commenting this out later and calling it after set max price probs
+		return CallQuery();
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
+
+	void SetOrigin(string o) {
+		origin = o;
+	}
+
+	void SetDuration(string d) {
+		duration = d;
+	}
+
+	void SetMaxPrice(string mp) {
+		max_price = mp;
+	}
+
+	IEnumerator CallQuery() {
+//		string monthVar = System.DateTime.Now.Month.ToString ();
+//		string dateVar = System.DateTime.Now.Date.ToString ();
+//		if (monthVar < 10)
+//			monthVar = "0" + monthVar;
+//		if (dateVar < 10)
+//			dateVar = "0" + dateVar;
+
+//		Debug.Log ("Pre using");
+
+		using (var wc = new System.Net.WebClient ()) {
+//			Debug.Log ("Post using");
+
+			url = "http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?" 
+				+ "apikey=" + apikey
+				+ "&origin=" + origin 
+				+ "&one-way=" + one_way 
+				+ "&duration=" + duration 
+				+ "&direct=" + direct
+				+ "&max_price=" + max_price;
+
+//			url = "http://api.sandbox.amadeus.com/v1.2/flights/" +
+//				"inspiration-search?apikey=5ydifrkNfmgRtCgp7lGDA4GYuOtsUlHG&" +
+//				"origin=LAX&one-way=false&duration=3&direct=true&max_price=100";
+			
+			var jsonString = wc.DownloadString (url);
+
+//			Debug.Log (jsonString);
+
+			q.CreateFromJson (jsonString);
+
+			int len;
+			if (q.results.Count < 4)
+				len = q.results.Count;
+			else
+				len = 4;
+		
+			for (int i = 0; i < len; i++) {
+				string cityurl = "https://api.sandbox.amadeus.com/v1.2/location/" 
+					+ q.results [i].destination
+					+ "?apikey="
+					+ apikey;
+
+				var tempJsonString = wc.DownloadString (cityurl);
+				var N = SimpleJSON.JSON.Parse (tempJsonString);
+
+				string city = N ["city"] ["name"];
+				destinations.Add (city);
+			}
+
+			WWW www = new WWW (url);
+			yield return www;
+
+		} 
+	}
+
 }
--- PeriTrip/Assets/query.cs
+++ PeriTrip/Assets/query.cs
@@ -1,19 +1,72 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using SimpleJSON;
+using System.Collections.Generic;
+//using Newtonsoft.Json.Linq;
 
 [Serializable]
-public class query {
-
-
-
-	// Use this for initialization
-	void Start () {
-	
+public class Query : MonoBehaviour {
+	public struct Result {
+		public string destination;
+		public string departure_date;
+		public string return_date;
+		public float price;
+		public string airline;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public bool exists;
+
+	public string origin;
+	public string currency;
+
+	public List<Result> results;
+
+	void Start() {
+		results = new List<Result> ();
 	}
+
+	// checks if query led to valid results
+	public bool JsonExists(string jsonString) {
+		var N = SimpleJSON.JSON.Parse (jsonString);
+
+		origin = N ["origin"].Value;
+
+		if (origin == null) {
+			exists = false;
+		}
+
+		exists = true;
+		return exists;
+	}
+
+	// will only be called if the result exists
+	public Query CreateFromJson(string jsonString) {
+		var N = SimpleJSON.JSON.Parse (jsonString);
+
+		origin = N ["origin"].Value;
+		currency = N ["currency"].Value;
+
+		JSONArray items = (JSONArray)N ["results"];
+
+//		Debug.Log (items.Count);
+
+		for (int i = 0; i < items.Count; i++)
+		{
+			Result r = new Result();
+
+			r.airline = N ["results"] [i] ["airline"];
+			r.departure_date = N ["results"] [i] ["departure_date"];
+			r.destination = N ["results"] [i] ["destination"];
+			r.return_date = N ["results"] [i] ["airline"];
+			r.price = float.Parse(N ["results"] [i] ["price"]);
+
+			results.Add (r);
+		}
+
+		return this;
+	}
+
+
+
 }
